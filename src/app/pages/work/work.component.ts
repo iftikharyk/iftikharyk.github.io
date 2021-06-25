3257c552d371566429c13d2e198a0b145fd1418a
Iftikhar Yar Khan <iftikharyk@Iftikhars-MacBook-Pro.local>
2021-06-26 00:28:49 +0500

    work also done

--- src/app/pages/work/work.component.ts
+++ src/app/pages/work/work.component.ts
@@ -10,7 +10,16 @@ export class WorkComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("logo").style.top = "10%";
+      } else {
+        document.getElementById("logo").style.top = "-10%";
+      }
+      prevScrollpos = currentScrollPos;
+    }
   }
 
 }
